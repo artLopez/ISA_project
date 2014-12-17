17a5013f028d1838a7815594e512308f6841f6e7
Arturo Lopez <arlopez@csumb.edu>
2014-12-16 22:05:44 -0800

    Finished benchmark2 and addedd benchmark#2 text file

--- src/ISA_project.java
+++ src/ISA_project.java
@@ -5,23 +5,24 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class Benchmark1 {
+public class ISA_project {
 	private static ArrayList<String> instructions;
+	private static ArrayList<String> arraysCreated;
 	private static HashMap<String, Double> doubleValues;
-	private static HashMap<String, String> variables;
+	private static int[] numArray = new int[5]; 
 	private static double reg1 = 0, reg2 = 0, reg3 = 0, sum = 0;
 	private static int difference = 0;
 	private static Scanner in;
 
 	public static void main(String[] args) {
 		instructions = new ArrayList<String>();
-		variables = new HashMap<String, String>();
 		doubleValues = new HashMap<String, Double>();
+		arraysCreated = new ArrayList<String>();
 		String key = "";
 
 		try {
 			FileReader fileIn = new FileReader(
-					"/Users/brian/Desktop/Benchmark#1.txt");
+					"fileName");
 			BufferedReader reader = new BufferedReader(fileIn);
 
 			while ((key = reader.readLine()) != null) {
@@ -91,9 +92,8 @@ public class Benchmark1 {
 						}
 					
 						if (instructions.get(t).contains("RESERVE"))
-						{
 							sum = sum + reg1;
-						}
+						
 					}
 				}
 				index += difference;
@@ -107,6 +107,64 @@ public class Benchmark1 {
 
 			if (instructions.get(index).substring(5, 8).equals("STOP"))
 				break;
+			
+			if(instructions.get(index).contains("PACK"))
+			{
+				String givenValues = instructions.get(index).substring(11);
+				
+				for (int i = 0; i < givenValues.length(); i++) {
+					if (givenValues.charAt(i) == ',') {
+						beg = givenValues.substring(0, i);
+						linesToRun = givenValues.substring(i + 2);
+						arraysCreated.add(beg);
+						break;
+					}
+				}
+					
+				line1 = linesToRun.substring(0, 3);
+				line2 = linesToRun.substring(4);
+					
+				difference = Integer.parseInt(line2) - Integer.parseInt(line1) + 1;
+				
+				for(int x = 0; x < instructions.size(); x++)
+				{
+					if(instructions.get(x).substring(5).equals(beg))
+					{
+						for(int indx = 0; indx < 5;indx++)
+							numArray[indx] = Integer.parseInt(instructions.get(x + indx + 1).substring(10));
+					}
+				}				
+			}
+			
+			if(instructions.get(index).contains("CHECK"))
+			{
+				String givenValues = instructions.get(index).substring(12);
+				boolean found = false;
+				
+				for (int i = 0; i < givenValues.length(); i++) {
+					if (givenValues.charAt(i) == ',') {
+						beg = givenValues.substring(0, i);
+						linesToRun = givenValues.substring(i + 2);
+						break;
+					}
+				}
+				if(arraysCreated.contains(beg))
+				{
+				
+					Integer i = doubleValues.get(linesToRun).intValue();
+					
+					for(int x = 0; x < 5;x++)
+					{
+						if(numArray[x] == i)
+							found = true;
+					}
+					
+					if(found)
+						System.out.println("The integer was found in the array!");
+					else
+						System.out.println("The integer was not found in the array!");
+				}
+			}
 		}
 	}
 
